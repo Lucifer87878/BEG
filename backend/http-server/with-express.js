930237c5402ca0acced2699323bfacadf4d89b6c
arvid1212 <145985929+arvid1212@users.noreply.github.com>
2024-10-21 13:36:58 +0200

    Events-express

---
+++ backend/http-server/with-express.js
@@ -0,0 +1,51 @@
+const express = require("express");
+
+const port = 3000;
+
+const server= express();
+
+/*
+/localhost300/
+    /events
+    /attendes
+    /user
+    /creator
+*/
+
+server.get("/events", (req,res)=> {
+
+    console.log("requist to",req.url)
+    res.send(events);
+
+
+});
+
+
+server.post("/events", (req,res)=> {
+
+    console.log("requist to",req.url)
+    events.push
+    ({
+     "name":"birthday",
+     "private":"false",
+     "descriptions":"birthday party"
+
+
+    });
+    try
+    {
+    events.push(req,body)
+    res.status(202);
+    res.send(events);
+    }
+    catch (err)
+    {
+      res.status(500);
+      res.send(events);
+    }
+
+});
+
+server.listen((port)  => {
+    console.log('server is running')
+});
